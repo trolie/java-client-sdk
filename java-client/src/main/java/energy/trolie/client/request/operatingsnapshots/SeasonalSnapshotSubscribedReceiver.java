f022c24176e85b63c3c3b4046d0c4a0ca5de7403
Samuel Dumitru <samuel-irinel.dumitru@gevernova.com>
2025-10-23 14:48:59 +0300

    [enh] fixing comments

--- java-client/src/main/java/energy/trolie/client/request/operatingsnapshots/SeasonalSnapshotSubscribedReceiver.java
+++ java-client/src/main/java/energy/trolie/client/request/operatingsnapshots/SeasonalSnapshotSubscribedReceiver.java
@@ -4,7 +4,7 @@ import energy.trolie.client.StreamingSubscribedResponseReceiver;
 import energy.trolie.client.exception.StreamingGetException;
 
 /**
- * Streaming receiver for updated forecast snapshot data and errors from subscriber.
+ * Streaming receiver for updated seasonal snapshot data and errors from subscriber.
  * The current request handling can be terminated in any of these methods by throwing an exception.
  * Errors originating from the subscriber thread will be sent to {@link #error(StreamingGetException)}
  */
